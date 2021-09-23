6f19b7ee833121594accd35a659a1a15bddfdf7b
nicasiomarques <nicasiomarques18@gmail.com>
2021-09-23 23:49:50 +0100

    refactor: transform normal selector to jquery selecto

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-const currentOperation = document.getElementById("currentOperation");
-const previewOperation = document.getElementById("previewOperation");
-const operations = Array("+", "-", "*", "/");
+const $ = document.querySelector.bind(document)
+const currentOperation = $('#currentOperation');
+const previewOperation = $('#previewOperation');
+const operations = ['+', '-', '*', '/'];
 
-function clear(currentOperation, previewOperation) {
-  currentOperation.value = "";
-  previewOperation.value = "";
+function dell(field) {
+  field.value = '';
 }
 
-function dell(value) {
-  currentOperation.value += "";
+function clear(currentOperation, previewOperation) {
+  dell(currentOperation)
+  dell(previewOperation)
 }
 
 function negative(currentOperation) {
@@ -19,16 +20,16 @@ function joinExpression(value) {
   currentOperation.value += value;
 }
 
-function calcAndShowcurrentOperation(previewOperation) {
+function calcAndShowCurrentOperation(previewOperation) {
   previewOperation.value = eval(previewOperation.value);
 }
 
-const isAction = (type) => type === "action";
-const clearPressed = (value) => value === "c";
-const equalsPressed = (value) => value === "=";
-const valuePressed = (value) => value === "value";
-const negativePressed = (value) => value === "negative";
-const dellPressed = (value) => value === "dell";
+const isAction = (type) => type === 'action';
+const clearPressed = (value) => value === 'c';
+const equalsPressed = (value) => value === '=';
+const valuePressed = (value) => value === 'value';
+const negativePressed = (value) => value === 'negative';
+const dellPressed = (value) => value === 'dell';
 
 function calc(type, value) {
   if (isAction(type)) {
@@ -44,25 +45,25 @@ function calc(type, value) {
 }
 
 // Change theme
-const button = document.querySelector(".js-theme");
+const button = $('.js-theme');
 const themes = {
-  "t-dark": "t-light",
-  "t-light": "t-dark",
+  't-dark': 't-light',
+  't-light': 't-dark',
 };
 
-const setDefaultTheme = (theme = "t-dark") =>
-  document.body.setAttribute("data-theme", themes[theme]);
+const setDefaultTheme = (theme = 't-dark') =>
+  document.body.setAttribute('data-theme', themes[theme]);
 
 setDefaultTheme();
 
-button.onclick = (event) => {
+button.onclick = () => {
   const currentTheme = document.body.dataset.theme;
-  document.body.setAttribute("data-theme", themes[currentTheme] || "t-dark");
+  document.body.setAttribute('data-theme', themes[currentTheme] || 't-dark');
 };
 
 // Change the icon of theme (Sun to moon)
-const light = document.getElementById("theme-icon");
+const light = document.getElementById('theme-icon');
 
-light.addEventListener("click", function (light) {
-  light.target.classList.toggle("dark-mode");
+light.addEventListener('click', function (light) {
+  light.target.classList.toggle('dark-mode');
 });
