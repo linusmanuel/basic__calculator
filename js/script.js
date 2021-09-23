a399b89fd3d32cac76a641849b7439e57a013f97
nicasiomarques <nicasiomarques18@gmail.com>
2021-09-24 00:14:33 +0100

    refactor: move theme to own file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 const $ = document.querySelector.bind(document)
-const currentOperation = $('#currentOperation');
-const previewOperation = $('#previewOperation');
-const operations = ['+', '-', '*', '/'];
+const currentOperation = $('.input__resulted')
+const previewOperation = $('.input__previous')
 
-function dell(field) {
-  field.value = '';
-}
+const OPERATIONS = ['+', '-', '*', '/']
+
+const dell = (field) => field.value = ''
 
 function clear(currentOperation, previewOperation) {
   dell(currentOperation)
@@ -13,57 +12,33 @@ function clear(currentOperation, previewOperation) {
 }
 
 function negative(currentOperation) {
-  currentOperation.value = -currentOperation.value;
+  currentOperation.value = -currentOperation.value
 }
 
 function joinExpression(value) {
-  currentOperation.value += value;
+  currentOperation.value += value
 }
 
 function calcAndShowCurrentOperation(previewOperation) {
-  previewOperation.value = eval(previewOperation.value);
+  previewOperation.value = eval(previewOperation.value)
 }
 
-const isAction = (type) => type === 'action';
-const clearPressed = (value) => value === 'c';
-const equalsPressed = (value) => value === '=';
-const valuePressed = (value) => value === 'value';
-const negativePressed = (value) => value === 'negative';
-const dellPressed = (value) => value === 'dell';
+const isAction = (type) => type === 'action'
+const clearPressed = (value) => value === 'c'
+const equalsPressed = (value) => value === '='
+const valuePressed = (value) => value === 'value'
+const negativePressed = (value) => value === 'negative'
+const dellPressed = (value) => value === 'dell'
 
 function calc(type, value) {
   if (isAction(type)) {
-    previewOperation.value = currentOperation.value;
-    if (clearPressed(value)) clear(currentOperation, previewOperation);
-    if (dellPressed(value)) dell(currentOperation);
-    if (operations.includes(value)) joinExpression(value);
-    if (negativePressed(value)) negative(currentOperation);
-    if (equalsPressed(value)) calcAndShowcurrentOperation(currentOperation);
+    previewOperation.value = currentOperation.value
+    if (clearPressed(value)) clear(currentOperation, previewOperation)
+    if (dellPressed(value)) dell(currentOperation)
+    if (OPERATIONS.includes(value)) joinExpression(value)
+    if (negativePressed(value)) negative(currentOperation)
+    if (equalsPressed(value)) calcAndShowCurrentOperation(currentOperation)
   }
 
-  if (valuePressed(type)) currentOperation.value += value;
+  if (valuePressed(type)) currentOperation.value += value
 }
-
-// Change theme
-const button = $('.js-theme');
-const themes = {
-  't-dark': 't-light',
-  't-light': 't-dark',
-};
-
-const setDefaultTheme = (theme = 't-dark') =>
-  document.body.setAttribute('data-theme', themes[theme]);
-
-setDefaultTheme();
-
-button.onclick = () => {
-  const currentTheme = document.body.dataset.theme;
-  document.body.setAttribute('data-theme', themes[currentTheme] || 't-dark');
-};
-
-// Change the icon of theme (Sun to moon)
-const light = document.getElementById('theme-icon');
-
-light.addEventListener('click', function (light) {
-  light.target.classList.toggle('dark-mode');
-});
